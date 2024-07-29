2e899503a7ddbc98aaed947d293fb2b8a024ded4
Neider <joshery@msn.com>
2024-07-29 09:22:32 -0600

    add linked in link and change name

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -16,6 +16,7 @@ export const config = { runtime: 'client' };
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Josh\s LinkedIn', href: 'https://www.linkedin.com/in/joshneider/', icon: UserGroupIcon },
   {
     name: 'Invoices',
     href: '/dashboard/invoices',
@@ -39,11 +40,12 @@ export default function NavLinks() {
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
+            {...(link.href.startsWith('http') ? { target: '_blank', rel: 'noopener noreferrer' } : {})}
           >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <LinkIcon className="h-6 w-6" />
+            {link.name}
           </Link>
         );
       })}
